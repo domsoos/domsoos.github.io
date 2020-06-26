6c19f86c132f2b3ecaddd42084a274788639a2e7
domsoos <63819914+domsoos@users.noreply.github.com>
2020-06-26 07:10:37 -0700

    slight

---
+++ script.js
@@ -0,0 +1,18 @@
+	var backNum = 0;
+
+	var change_backgrounds = [["imgs/background.png"]];
+
+	document.getElementById("project_button").onclick = function() {projects()};
+
+	function projects() {
+		alert("yellow");
+		//location.href = "https://domsoos.github.io/projects";
+
+		var url = "https://domsoos.github.io/projects";
+		window.location(url);
+		
+	}
+
+	document.getElementById("hello").onclick = function() {
+		alert("Yellooowww");
+	}
