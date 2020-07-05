9739ee9b02fb03d2aa4e36930f3d8f17433e0dde
domsoos <63819914+domsoos@users.noreply.github.com>
2020-07-04 20:02:16 -0700

    front end

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 	var backNum = 0;
 
-	var change_backgrounds = [["imgs/background.png"]];
+	var change_backgrounds = [["imgs/background.png", "", ]];
 
 	document.getElementById("project_button").addEventListener("click", function() {
 		location.href = "https://domsoos.github.io/projects";
@@ -18,7 +18,35 @@
 		//window.location(url);
 		
 	}
+	//Initializing
+var i = 0;
+var images = []; //array
+var time = 3000; // time in millie seconds
 
+//images
+
+images[0] = "url(./Images/1.jpg)";
+images[1] = "url(./Images/2.jpg)";
+images[2] = "url(./Images/3.jpg)";
+//function
+
+function changeImage() {
+    var el = document.getElementById('body');
+    el.style.backgroundImage = images[i];
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout('changeImage()', time);
+}
+
+window.onload = changeImage;
+	function backgroundChanger() {
+  		var x = Math.floor((Math.random() * 5) + 1);
+  		
+
+  	}
 
 
 
