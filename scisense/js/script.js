33450b5b0f4dcb1f5689518897a6442b5455edb8
Dominik Soós <soos.domi@gmail.com>
2024-11-08 18:46:00 -0500

    user sign in

--- scisense/js/script.js
+++ scisense/js/script.js
@@ -122,6 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
   emailSignInForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;//.trim();
+    
     const password = document.getElementById('password').value;
 
     auth.signInWithEmailAndPassword(email, password)
